61ed431b98ca44d153ed496a97955560726a2ade
gaurav munankarmi <96043792+gaurab7@users.noreply.github.com>
2025-07-14 20:04:57 +0545

    did nothing

---
+++ Controllers/msg-controllers.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace moist_von_lipwig.Controllers
+{
+    [ApiController]
+    [Route("lipwig/[controller]")]
+    public class MsgController
+    {
+       
+    }
+}
--- Models/user.cs
+++ Models/user.cs
@@ -4,7 +4,9 @@ namespace moist_von_lipwig.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public required string UserName { get; set; }
         public required string Email { get; set; }
 
