081cc3106007531264509a51a15d5186052d9660
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-09-14 03:21:39 +0000

    login & style

--- src/server.js
+++ src/server.js
@@ -11,10 +11,12 @@ const __dirname = path.dirname(__filename)
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')))
 const PORT = process.env.PORT || 8848
 
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/login.html'))
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
